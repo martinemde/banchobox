709858d8532f57bab9a94076559ebd4e4d4cb7e7
Martin Emde <me@martinemde.com>
2025-08-30 21:13:21 -0700

    Improve the sidebar

--- src/lib/ui/HiredStaffSidebar.svelte
+++ src/lib/ui/HiredStaffSidebar.svelte
@@ -17,15 +17,46 @@
 		information. Use this to keep track of your hired staff and their costs.
 	</p>
 {:else}
-	<ul class="space-y-2">
-		{#each hired as h (h.id)}
-			<li class="rounded-lg border border-surface-500 p-2">
-				<div class="truncate text-sm font-medium">{h.name}</div>
-				<div class="mt-1 flex items-center justify-between text-xs opacity-70">
-					<span>Wage {h.wage.toLocaleString()}</span>
-					<button class="underline" onclick={() => emit(h.id)}>Unhire</button>
-				</div>
-			</li>
-		{/each}
-	</ul>
+	<div class="table-wrap">
+		<table class="table caption-bottom">
+			<caption class="pt-4">Hired Staff</caption>
+			<thead>
+				<tr>
+					<th class="p-1 text-left">Name</th>
+					<th class="p-1 text-left">Level</th>
+					<th class="p-1 text-right">Wage</th>
+					<th class="p-1 text-right">&nbsp;</th>
+				</tr>
+			</thead>
+			<tbody class="[&>tr]:hover:preset-tonal-secondary">
+				{#each hired as h (h.id)}
+					<tr>
+						<th class="truncate p-1 text-sm font-medium">{h.name}</th>
+						<td class="p-1 text-right text-sm tabular-nums">
+							{h.level ?? '20'}
+						</td>
+						<td class="p-1 text-right text-sm tabular-nums">
+							{h.wage.toLocaleString()}
+						</td>
+						<td class="p-1 text-right">
+							<button
+								class="btn btn-sm hover:preset-filled-tertiary-700-300"
+								onclick={() => emit(h.id)}>Dismiss</button
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+			<tfoot>
+				<tr>
+					<td class="p-1">&nbsp;</td>
+					<td class="p-1 text-right tabular-nums">Total</td>
+					<td class="p-1 text-right tabular-nums">
+						{hired.reduce((acc, h) => acc + h.wage, 0).toLocaleString()}
+					</td>
+					<td class="p-1 text-right">&nbsp;</td>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
 {/if}
